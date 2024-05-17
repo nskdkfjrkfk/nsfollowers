8302d12be46b3569fdcaffc0d8a5516a255511e1
nskdkfjrkfk <pazzoperilmare@gmail.com>
2024-05-17 17:20:28 +0530

    Update Ns_Followers.py

--- Ns_Followers.py
+++ Ns_Followers.py
@@ -11,9 +11,9 @@ class Ns_Followers:
         self.requests=requests
         self.__AES_NoPadding = AES_CBC_NoPadding()
 
-        self.__userid='...' #Enter UserID Here
-        self.__username='...' #Enter UserName Here
-        self._session='...' #Enter SessionID Here
+        self.__userid='3242860753' #Enter UserID Here
+        self.__username='enrico5220' #Enter UserName Here
+        self._session='3242860753:fWVxf08DeJGuBB:12:AYcrqTSCXtBUT4vj37WFYYoOVy92NzsPUHcdQfGjag' #Enter SessionID Here
 
         self.__Pk=self.__userid.encode()+b'*'+str(random.randint(111,999)).encode()
         self.__Ip=os.urandom(8).hex().encode()+b'*'+str(random.randint(111,999)).encode()
